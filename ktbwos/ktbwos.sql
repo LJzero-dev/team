b012510b3a57948376ac33f3a94111052dfa8885
LJzero-dev <ljzero.dev@gmail.com>
2023-06-21 11:55:21 +0900

    .

--- ktbwos/ktbwos.sql
+++ ktbwos/ktbwos.sql
@@ -96,7 +96,7 @@ create table t_free_reply (
 select * from t_free_reply;
 insert into t_free_reply (fl_idx, fr_writer, fr_content, fr_ip) values (1, '홍길동', '댓글 1번입니다.', '127.0.0.0');
 
--- drop table t_pds_list;
+drop table t_pds_list;
 create table t_pds_list (
    pl_idx int primary key auto_increment,	-- 글번호
    ai_idx int not null,						-- 관리자번호
@@ -105,12 +105,14 @@ create table t_pds_list (
    pl_data1 varchar(100) not null,			-- 파일1
    pl_data2 varchar(100),					-- 파일2
    pl_read int default 0,					-- 조회수
+   pl_isview char(1) default 'y',			-- 게시 여부
    pl_date datetime default now(),			-- 작성일
     constraint fk_pds_list_ai_idx foreign key(ai_idx) references t_admin_info(ai_idx)
 );
 
 insert into t_pds_list (ai_idx, pl_title, pl_content, pl_data1, pl_data2) values (1, '자료글 1번글입니다.', '자료글 2번글입니다.', '../pds/c.png', '../pds/d.png');
 
+drop table t_request_list;
 select * from t_request_list;
 create table t_request_list (
    rl_idx int primary key auto_increment,		-- 게시판 번호
@@ -133,16 +135,24 @@ select * from t_request_list;
 select * from t_request_list  where 1=1  and rl_title like '%1%' order by rl_status ;
 insert into t_request_list (rl_ctgr, rl_title, rl_name, rl_writer, rl_write, rl_reply_use, rl_reply_write, rl_content) values ('a', '홍길동 키우기 게시판 요청합니다', '홍길동 키우기', '전우치', 'y', 'y', 'y', '만들어 주세요');
 
+-- drop table t_best_list;
+select * from t_best_list;
 create table t_best_list (
 	bl_idx int primary key auto_increment,	-- 인기 게시판 일련번호
-	rl_idx int not null,					-- 게시판 번호
+	bl_table_name varchar(20) not null,		-- 게시판 테이블 이름
 	rl_name varchar(50) not null,			-- 게시판 이름
-	bl_count int default 0,					-- 게시글 조회수 합
+    bl_count int default 1,					-- 게시글 조회 회수
 	bl_date datetime default now(),			-- 게시판 조회일
-    constraint fk_best_list_rl_idx foreign key(rl_idx) references t_request_list(rl_idx),
-    constraint fk_best_list_rl_name foreign key(rl_name) references t_request_list(rl_name)
+	constraint fk_best_list_rl_name foreign key(rl_name) references t_request_list(rl_name)
 );
+select * from t_best_list;
+select * from t_request_list;
+insert into t_best_list (bl_table_name, rl_name) values ('ball', '야구');
+insert into t_best_list (bl_table_name, rl_name) values ('', '');
+select bl_count from t_best_list where bl_table_name = 'ball' and date(bl_date) = date(now());
+update t_best_list set bl_count = bl_count + 1 where bl_table_name = 'ball' and date(bl_date) = date(now());
 
+select * from t_best_list;
 select bl_date from t_best_list where bl_date < left(now(), 7) and bl_date > left(DATEADD(DAY, -10, now()), 7);
 
 update t_member_info set mi_count = mi_count + 1 where mi_nick = '전우치';
@@ -154,6 +164,8 @@ select * from (
     select * from t_tree_list
 ) a where br_title like '%검색어%';
 
+use ktbwos;
+show tables;
 select * from t_request_list;
 show tables;
 select * from t_member_info;
@@ -164,6 +176,82 @@ insert into t_member_info (mi_id,mi_email,mi_nick,mi_pw,mi_status,mi_reason) val
 insert into t_member_info (mi_id,mi_email,mi_nick,mi_pw,mi_status,mi_reason) values ('date5','date5@naver.com','e홍','9999','c','신규');
 select * from t_member_info   where mi_status = 'all' and mi_nick like '%꺽%';
 select * from t_member_info   where mi_status != 'all' and mi_nick like '%꺽%';
+
+select * from t_notice_list;
+select * from t_admin_info;
+insert into t_notice_list (ai_idx ,nl_title, nl_content) values ('1','공지 제목','공지 내용');
+
+select * from t_request_list;
+update t_request_list set rl_read = rl_read + 1 where rl_table_name = '';
+select * from t_member_info;
+select rl_ctgr, rl_name, rl_table_name from t_request_list where rl_status = 'y' order by rl_read desc limit 10;
+select nl_idx, nl_title, date(nl_date) from t_notice_list;
+select ai_iax, ;
+show tables;
+select * from t_request_list;
+select rl_name, from t_request_list where rl_status = 'y' order by rl_idx desc limit 10;
+select * 
 select * from t_aa_reply;
 
 update t_requset_list set rl_status = 'n', rl_title = '', rl_reason = '' where fr_idx ='';
+
+
+select * from t_best_list;
+select * from t_121212_list;
+update t_best_list set bl_count = bl_count - '' where bl_table_name = '' and date(bl_date) = date('') ;
+
+
+select * from t_request_list;
+select * from t_best_list;
+select * from t_ball_list;
+
+select * from t_request_list;
+show tables;
+
+
+show tables;
+
+select 121212_read from t_121212_list where 121212_idx = 1;
+
+
+
+
+create table t_request_list (
+   rl_idx int primary key auto_increment,		-- 게시판 번호
+   rl_ctgr char(1) default 'a',					-- 게시판 테마 분류
+   rl_title varchar(50) not null,				-- 요청 제목
+   rl_name varchar(50) unique not null,			-- 게시판 이름
+   rl_writer varchar(20) not null,				-- 회원 닉네임
+   rl_write char(1) not null,					-- 글작성 권한여부
+   rl_reply_use char(1) not null,				-- 댓글 사용 여부
+   rl_reply_write char(1),						-- 댓글 작성 권한
+   rl_isview char(1) default 'y',				-- 글 노출 여부
+   rl_table_name varchar(20),					-- 테이블 이름
+   rl_status char(1) default 'a',				-- 승인여부
+   rl_reason varchar(50),						-- 미승인사유
+   rl_content text not null,					-- 요청 내용
+   rl_date datetime default now()				-- 요청일
+);
+
+create table t_best_list (
+	bl_idx int primary key auto_increment,	-- 인기 게시판 일련번호
+	bl_table_name varchar(20) not null,		-- 게시판 테이블 이름
+	rl_name varchar(50) not null,			-- 게시판 이름
+    bl_count int default 1,					-- 게시글 조회 회수
+	bl_date datetime default now(),			-- 게시판 조회일
+	constraint fk_best_list_rl_name foreign key(rl_name) references t_request_list(rl_name)
+);
+
+select a.rl_ctgr, a.rl_name, a.rl_table_name
+from t_request_list a inner join t_best_list b on a.rl_table_name = b.bl_table_name
+where a.rl_status = 'y' and date(b.bl_date) > date_add(date(now()), interval -30 day)
+group by a.rl_ctgr, a.rl_name, a.rl_table_name order by sum(b.bl_count) desc;
+
+
+select * from t_member_info;
+
+
+
+
+
+
